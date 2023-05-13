903263dc002e320327480b2f173a52a15b5a6ddd
MatwejVasilchenko <110052092+MatwejVasilchenko@users.noreply.github.com>
2023-05-13 20:49:57 +0300

    all work

--- 6JSHM8.js
+++ 6JSHM8.js
@@ -1,2 +1,6 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(Math.max([1], [2], [3], [4], [5], [6], [7], [8], [9], [10]))
+let numbers = [1, 2, 3, 4, 5, 6, 7, 99, 9, 10];
+let sumNumbers = 0;
+for (let i = 0; i < numbers.length; i++){
+    sumNumbers += numbers[i]
+}
+console.log(sumNumbers);
---
+++ 7JSHM8.js
@@ -0,0 +1,7 @@
+const numbers = [30, 100, 45, 76, 234, 98, 11, 33, 44, 55];
+for(let i = 0; i = purchases.length; i++){
+    if (purchases[i] % 2 === 0){
+       alert(`${i} парне`)
+       console.log(i)
+    }
+}
